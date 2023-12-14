3605691a1792d59d2065daf894bac4f101f1ec7b
felle pelle <realcrapshit@gmail.com>
2023-12-14 15:11:52 +0300

    feat: Add TS scripts to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AdvantagesType} from "./types/advantages.type";
+import {MacaroonsType} from "./types/macaroons.type";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'macaroons';
+  public selectedProduct: MacaroonsType | null = null;
+  public phoneNumber: string = '+375 (29) 368-98-68';
+  public instagramLink: string = 'https://www.instagram.com/';
+
+  public advantages: AdvantagesType[] = [
+    {
+      title: 'Лучшие продукты',
+      description: 'Мы честно готовим макаруны только из натуральных и качественных продуктов. Мы не используем консерванты, ароматизаторы и красители.',
+    },
+    {
+      title: 'Много вкусов',
+      description: 'Наша задача – предоставить вам широкое разнобразие вкусов. Вы удивитесь, но у нас более 70 вкусов пироженок.',
+    },
+    {
+      title: 'Бисквитное тесто',
+      description: 'Все пирожные готовятся на бисквитном тесте с качественным сливочным маслом 82,5%. В составе нет маргарина и дрожжей!',
+    },
+    {
+      title: 'Честный продукт',
+      description: 'Вкус, качество и безопасность наших пирогов подтверждена декларацией о соответствии, которую мы получили 22.06.2016 г.',
+    },
+  ];
+
+  public macaroons: MacaroonsType[] = [
+    {
+      image: '1.png',
+      name: 'Макарун с малиной',
+      quantity: 1,
+      price: 1.70,
+    },
+    {
+      image: '2.png',
+      name: 'Макарун с манго',
+      quantity: 1,
+      price: 1.70,
+    },
+    {
+      image: '3.png',
+      name: 'Пирог с ванилью',
+      quantity: 1,
+      price: 1.70,
+    },
+    {
+      image: '4.png',
+      name: 'Пирог с фисташками',
+      quantity: 1,
+      price: 1.70,
+    },
+  ];
+
+  public showPresent: boolean = true;
+
+  public formatPrice(price: number): string {
+    return price.toFixed(2).replace('.', ',');
+  }
+
+  public scrollTo(target: HTMLElement | null): void {
+    target?.scrollIntoView({behavior: 'smooth'});
+  }
+
+  public orderProduct(product: MacaroonsType, target: HTMLElement | null): void {
+    this.selectedProduct = product;
+
+    const inputProduct = document.getElementById('inputProduct') as HTMLInputElement;
+    if (inputProduct) {
+      inputProduct.value = product.name.toUpperCase();
+    }
+
+    this.scrollTo(target);
+  }
+
 }
---
+++ src/app/types/macaroons.type.ts
@@ -0,0 +1,6 @@
+export type MacaroonsType = {
+  image: string,
+  name: string,
+  quantity: number,
+  price: number,
+}
